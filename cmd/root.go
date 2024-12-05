6a20347277d3928459f179458ce346d9c7a03f63
Joseph G. Pawletko <jgpawletko@gmail.com>
2024-12-04 20:39:55 -0500

    add subcommand help functions to root.go

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
@@ -10,4 +11,19 @@ var rootCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("** ASPACE DO TOOLS **")
 	},
+}
+
+// print help
+func printNeedSubcommandHelp(cmd *cobra.Command) {
+	str := getUseSequence(cmd)
+	fmt.Printf("Error: missing subcommand.\nFor options, please type: %s -h\n", str)
+}
+
+func getUseSequence(cmd *cobra.Command) string {
+	var seq string
+
+	if cmd.HasParent() {
+		seq = getUseSequence(cmd.Parent()) + " "
+	}
+	return seq + cmd.Use
 }
